0a0cfc3ed5d794dc349eb00354d823de497cec5e
ts24corp <play@ts24.com.vn>
2019-10-09 14:24:05 +0700

    up

--- lib/src/app/models/parent.dart
+++ lib/src/app/models/parent.dart
@@ -1,13 +1,13 @@
-class Driver {
+class Parent {
   int id;
   String name;
   String photo;
   String gender;
   String phone;
 
-  Driver(this.id, this.name, this.photo, this.gender, this.phone);
+  Parent(this.id, this.name, this.photo, this.gender, this.phone);
 
-  Driver.fromJson(Map<dynamic, dynamic> json) {
+  Parent.fromJson(Map<dynamic, dynamic> json) {
     id = json['id'];
     name = json['name'];
     photo = json['photo'];
@@ -25,18 +25,12 @@ class Driver {
     return data;
   }
 
-  static final List<Driver> list = [
-    Driver(
+  static final info = [
+    Parent(
         1,
         'Driver 1',
         "https://shalimarbphotography.com/wp-content/uploads/2018/06/SBP-2539.jpg",
         'F',
         "0907488458"),
-    Driver(
-        2,
-        'Driver 2',
-        "https://shalimarbphotography.com/wp-content/uploads/2018/06/SBP-0800.jpg",
-        'F',
-        "0905123456"),
   ];
 }
