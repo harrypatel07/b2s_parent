cc260865bde6a55e65a22f5c9d9878dbb3917c82
ts24corp <play@ts24.com.vn>
2020-07-27 11:38:39 +0700

    up

--- android/app/src/main/kotlin/b2sparent/ts24corp/com/b2s_parent/MainActivity.kt
+++ android/app/src/main/kotlin/b2sparent/ts24corp/com/b2s_parent/MainActivity.kt
@@ -1,13 +1,11 @@
 package b2sparent.ts24corp.com.b2s_parent
 
-import android.os.Bundle
-
-import io.flutter.app.FlutterActivity
+import io.flutter.embedding.android.FlutterActivity
+import androidx.annotation.NonNull;
+import io.flutter.embedding.engine.FlutterEngine
 import io.flutter.plugins.GeneratedPluginRegistrant
-
 class MainActivity: FlutterActivity() {
-  override fun onCreate(savedInstanceState: Bundle?) {
-    super.onCreate(savedInstanceState)
-    GeneratedPluginRegistrant.registerWith(this)
-  }
+        override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+        GeneratedPluginRegistrant.registerWith(flutterEngine);
+    }
 }
